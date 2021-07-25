dae7b1bc235244c99c18d3ddf520fd9afcf61031
nail11 <nail@t-online.hu>
2021-07-25 21:24:39 +0200

    első automatizált teszt

---
+++ conduit tesztek/kornyezet.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import time
+from random import randint
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+
+url = "http://localhost:1667/"
+
+driver.get(url)
+home_window = driver.window_handles[0]
+butts = driver.find_elements_by_xpath('//*[@id="app"]/nav/div/ul/li/a')
+
+
+def main_butts_activate(xpath):
+
+    time.sleep(2)
+    xpath.click()
+    time.sleep(3)
+    assert driver.current_url == xpath.get_attribute("href")
+    driver.switch_to_window(home_window)
+
+for i, button in enumerate(butts):
+    main_butts_activate(butts[i])
+
+
+
+#driver.close()
