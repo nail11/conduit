0c70d32d2da0ea79cf5bc5023352e4f989b3b59f
nail11 <nail@t-online.hu>
2021-08-12 21:36:56 +0200

    "new entry" teszt kész

--- conduit tesztek/test_new_entry.py
+++ conduit tesztek/test_new_entry.py
@@ -18,8 +18,8 @@ import time
 
 options = Options()
 
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
@@ -30,38 +30,74 @@ url = "http://localhost:1667/#/editor"
 
 driver.get(url)
 
-main_butts = driver.find_elements_by_xpath('//*[@id="app"]/nav/div/ul/li/a')
-main_butts[2].click()
+# main_butts = driver.find_elements_by_xpath('//*[@id="app"]/nav/div/ul/li/a')
+# main_butts[2].click()
 
 # data - elements, variables etc.
 
-    # elements
+# elements
 
-article_title_path = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[1]/input'
-article_summ_path = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[2]/input'
-article_body_path = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[3]/textarea'
-article_tag_path = '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[4]/div/div/ul/li/input'
+# input elements
 
-    # messages
+art_title_text = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[1]/input')
+art_summ_text = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[2]/input')
+art_body_text = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[3]/textarea')
+art_tag_text = driver.find_element_by_xpath(
+    '//*[@id="app"]/div/div/div/div/form/fieldset/fieldset[4]/div/div/ul/li/input')
+pub_butt = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
 
-user_req = "Username field required."
-email_req = "Email field required."
-pass_req = "Password field required."
-invalid_email = "Email must be a valid email."
-invalid_pass = "Password must be 8 characters long and include 1 number, 1 uppercase letter, " \
-               "and 1 lowercase letter."
-reused_email = "Email already taken."
-signup_success = "Your registration was successful!"
+# saved text containers
 
-    # variables
+title_cont_path = '/html/body/div[1]/div/div[1]/div/h1'
+main_text_cont_path = '//*[@id="app"]/div/div[2]/div[1]/div/div[1]/p'
+tag_cont_path = '//*[@id="app"]/div/div[2]/div[1]/div/div[2]/a'
 
-field_val = []  # a list of "empty", "good", "bad" or "reused" according to the values
-                # one would like to give the input fields. The order is "username", "email" and "password".
-warning_title = "" # the title of the message window
-warning_text = ""  # warning or message in the jumping up window
+# messages
 
-# elements on the "sign_up" page
+message01 = "Article title: "
+message02 = "Article body text: "
+message03 = "Article tag: "
+message1 = "The saved (entry) data and the test data are identical !"
+message2 = "Test passed ! "
+err_message1 = "Test failed ! The saved (entry) data and the test data are not identical !"
 
-elements_xpath = '//*[@id="app"]/div/div/div/div/form/'
-input_xpath = elements_xpath + 'fieldset/input'
-signup_butt = driver.find_element_by_xpath(elements_xpath + 'button')
+# test data
+
+title_text = "A Tisza"
+summ_text = "Petőfi Sándor verse"
+main_text = "\n Nyári napnak alkonyúlatánál.\n Megállék a kanyargó Tiszánál\n Ott, hol a kis Túr " \
+            "siet beléje,\n Mint a gyermek anyja kebelére."
+tag_text = "vers"
+
+
+# functions
+
+def element_by_path(xpath):
+    element = driver.find_element_by_xpath(xpath)
+    return element
+
+
+def text_feeding_in(text, field):
+    for letter in text:
+        field.send_keys(letter)
+
+
+# Test
+
+text_feeding_in(title_text, art_title_text)
+text_feeding_in(summ_text, art_summ_text)
+text_feeding_in(main_text, art_body_text)
+text_feeding_in(tag_text, art_tag_text)
+pub_butt.click()
+time.sleep(1)
+assert title_text == element_by_path(title_cont_path).text, err_message1
+print(message01, message1)
+main_text1 = main_text.replace('\n', "")
+assert main_text1.strip() == element_by_path(main_text_cont_path).text, err_message1
+print(message02, message1)
+assert tag_text == element_by_path(tag_cont_path).text, err_message1
+print(message03, message1)
+print()
+print(message2)
+
+driver.close()
