7917189c80fd553f1e0d77d38e0242df3c887772
nail11 <nail@t-online.hu>
2021-08-13 16:56:21 +0200

    conduitdata - collected data and functions

---
+++ conduit tesztek/conduitdata.py
@@ -0,0 +1,24 @@
+# General elements, variables and functions for Conduit application testing
+input_butt_path = '//*[@id="app"]/div/div/div/div/form/button'
+
+def element_by_path(xpath):
+    element = driver.find_element_by_xpath(xpath)
+    return element
+
+def input_elements_list(xpath):
+    input_elements = driver.find_elements_by_xpath(xpath)
+    return input_elements
+
+    # finding input element's names for further use (in an other app)
+def input_elements_names(element_list):
+    input_names = []
+    for i in range(len(element_list)):
+        input_name = input_elements[i].get_attribute("placeholder")
+        input_names.append(input_name)
+    return input_names
+
+def sign_in():
+    element_by_path(email_input_path).send_keys(email)
+    element_by_path(password_input_path).send_keys(passwd)
+    element_by_path(input_butt_path).click()
+    time.sleep(1)
